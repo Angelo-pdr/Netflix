8e663c35523ef38d96da65566f3c46a67ea351de
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-06-01 10:47:00 -0300

    adicionado as section da home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,6 +11,21 @@ export const Home = () => {
                 <div className="gradient"></div>
                 <FormCadastro/>
             </div>
+            <section className="grid">
+                <div>
+                    <h1>Aproveite na TV.</h1>
+                    <p>Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray
+                     e outros dispositivos.</p>
+                </div>
+                <div className="imgtv">
+                    <img src="../src/img/tv.png" alt="imagem-da-televisão" />
+                    <video className="video-tv" autoPlay playsInline muted loop>
+                        <source 
+                            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
+                        />
+                    </video>
+                </div>
+            </section>
         </>
     )
 
