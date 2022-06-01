2e4a0ccd09988447aa2425b3e1c0ee993f0560c8
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-06-01 11:32:03 -0300

    estilzando as section da homem

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,24 +7,28 @@ export const Home = () => {
     return(
         <>
             <AreaHeader/>
-            <div className="container">
+            <div className="main">
                 <div className="gradient"></div>
                 <FormCadastro/>
             </div>
-            <section className="grid">
-                <div>
-                    <h1>Aproveite na TV.</h1>
-                    <p>Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray
-                     e outros dispositivos.</p>
-                </div>
-                <div className="imgtv">
-                    <img src="../src/img/tv.png" alt="imagem-da-televisão" />
-                    <video className="video-tv" autoPlay playsInline muted loop>
-                        <source 
-                            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
-                        />
-                    </video>
-                </div>
+            <section className="section">
+              <div className="container">
+                    <div className="grid">
+                        <div>
+                            <h1>Aproveite na TV.</h1>
+                            <p>Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray
+                                e outros dispositivos.</p>
+                        </div>
+                        <div className="img-tv">
+                            <img src="../src/img/tv.png" alt="imagem-da-televisão"  />
+                            <video className="video-tv" autoPlay playsInline muted loop>
+                                    <source 
+                                    src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v"
+                                    type="video/mp4"/>
+                            </video>
+                        </div>
+                    </div>
+              </div>
             </section>
         </>
     )
