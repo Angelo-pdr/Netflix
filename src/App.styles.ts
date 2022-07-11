93025387a6f9ab518eb4a8b0d653a23cccf7c5a7
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-07-11 13:50:25 -0300

    banner estilizado

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container =styled.div`
-    width: 100%;
-    height: 100%;
+    max-width: 100vw;
+    max-height: 100vh;
     background-color: black;
     font-family: sans-serif;
 `
