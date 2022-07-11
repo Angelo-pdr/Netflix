ff99de6b2b4e3f1f4ed5ec74338979c3345a9164
Angelo Pedrosa <angelo.ppedrosa@gmail.com>
2022-07-11 13:35:29 -0300

    configurando o banner do css

--- src/pages/memberArea/styles.ts
+++ src/pages/memberArea/styles.ts
@@ -3,14 +3,14 @@ type BannerType = {
     banner: string
 }
 export const Container = styled.div`
-    min-width: 100%;
+    max-width: 100vw;
     height: 100vh;
     background-color: black;
     padding-bottom: 50px;
 `
 
 export const Destaques = styled.div<BannerType>`
-    height: 100vh;
+    height: 100%;
     background-size: 'cover';
     background-position: 'center';
     background-image: url(${props => props.banner});
